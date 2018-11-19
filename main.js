ecb759ad87f6e33037d07c72321bd9066de540e3
Bart <bartst010395@gmail.com>
2018-11-19 12:06:37 +0100

    js refactoring

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function tasksNumber() {
     tasksNumber.innerText = 'any';
   } else {
     tasksNumber.innerText = data.todo.value.length;
-  }
+  };
 };
 
 // === RENDER TODO LIST FROM LOCALSTORAGE ===
@@ -60,12 +60,12 @@ const categoryInputs = document.querySelector('.categories').querySelectorAll('i
 // === ADD EVENT LISTENER FOR ADD BUTTON ===
 document.querySelector('.add-btn').addEventListener('click', createItem);
 
+// === CREATE ITEM AND PUSH IT TO THE LIST ===
 function createItem() {
   let value = document.querySelector('#add-item').value;
   let description = document.querySelector('#add-description').value;
   if (description === '') description = 'A comment has not been added.';
   let icon;
-  let categoryInputs = document.querySelector('.categories').querySelectorAll('input');
   let homeIcon = '<i class="fas fa-home"></i>';
   let workIcon = '<i class="fas fa-briefcase"></i>';
   let schoolIcon = '<i class="fas fa-graduation-cap"></i>';
@@ -94,7 +94,7 @@ function createItem() {
         case 'others':
           icon = othersIcon;
           break;
-          // default not work?? why?
+        // default not work?? why?
         default:
           icon = othersIcon;
       }
@@ -106,6 +106,18 @@ function createItem() {
   if (value) addItem(value, description, categoryIcon);
 }
 
+// === LOOP FUNCTIONS ===
+function loopForSplice(list, nodes) {
+  Object.entries(list).forEach((arr, index) => {
+    arr[1].splice(arr[1].indexOf(nodes[index]), 1);
+  });
+};
+function loopForPush(list, nodes) {
+  Object.entries(list).forEach((arr, index) => {
+    arr[1].push(nodes[index]);
+  });
+}
+
 // === REMOVE ITEM ===
 function removeItem() {
   let item = this.parentNode.parentNode;
@@ -115,18 +127,15 @@ function removeItem() {
   let value = item.childNodes[0].childNodes[1].childNodes[0].innerText;
   let description = item.childNodes[0].childNodes[2].childNodes[0].innerText;
   let icon = item.childNodes[0].childNodes[0].childNodes[1].innerHTML;
+  const nodes = [];
+  nodes.push(value, description, icon);
 
   if (id === 'todo') {
-    data.todo.value.splice(data.todo.value.indexOf(value), 1);
-    data.todo.description.splice(data.todo.description.indexOf(description), 1);
-    data.todo.icon.splice(data.todo.icon.indexOf(icon), 1);
+    loopForSplice(data.todo, nodes);
   } else {
-    data.complited.value.splice(data.complited.value.indexOf(value), 1);
-    data.complited.description.splice(data.complited.description.indexOf(description), 1);
-    data.complited.icon.splice(data.complited.icon.indexOf(icon), 1);
-  }
+    loopForSplice(data.complited, nodes);
+  };
   dataObjectUpdated();
-
   parent.removeChild(item);
 };
 
@@ -139,22 +148,16 @@ function completeItem() {
   let value = item.childNodes[0].childNodes[1].childNodes[0].innerText;
   let description = item.childNodes[0].childNodes[2].childNodes[0].innerText;
   let icon = this.nextElementSibling.innerHTML;
+  const nodes = [];
+  nodes.push(value, description, icon);
 
-  // Remove item from todo list and push it to completed list
+  // Remove item from todo list and push it to completed list (and vice versa)
   if (id === 'todo') {
-    data.todo.value.splice(data.todo.value.indexOf(value), 1);
-    data.todo.description.splice(data.todo.description.indexOf(description), 1);
-    data.todo.icon.splice(data.todo.icon.indexOf(icon), 1);
-    data.complited.value.push(value);
-    data.complited.description.push(description);
-    data.complited.icon.push(icon);
+    loopForSplice(data.todo, nodes);
+    loopForPush(data.complited, nodes);
   } else {
-    data.complited.value.splice(data.complited.value.indexOf(value), 1);
-    data.complited.description.splice(data.complited.description.indexOf(description), 1);
-    data.complited.icon.splice(data.complited.icon.indexOf(icon), 1);
-    data.todo.value.push(value);
-    data.todo.description.push(description);
-    data.todo.icon.push(icon);
+    loopForSplice(data.complited, nodes);
+    loopForPush(data.todo, nodes);
   }
   dataObjectUpdated();
 
@@ -171,101 +174,85 @@ function addItem(value, description, categoryIcon) {
   document.querySelector('#add-description').value = '';
   for (let i = 0; i < categoryInputs.length; i++) {
     if (categoryInputs[i].checked) categoryInputs[i].checked = false;
-  }
+  };
   addPanel.classList.remove('opened');
 
-  data.todo.value.push(value);
-  data.todo.description.push(description);
-  data.todo.icon.push(categoryIcon);
-
+  const nodes = [value, description, categoryIcon];
+  loopForPush(data.todo, nodes);
   dataObjectUpdated();
 };
 
 // === ADD A NEW ITEM TO THE DOM ===
 // if complited = true - item will be added to complited list (if false: todo)
 function addItemToDOM(text, description, categoryIcon, complited) {
-  let list = (complited) ? document.getElementById('completed') : document.getElementById('todo');
+  const list = (complited) ? document.getElementById('completed') : document.getElementById('todo');
 
-  let item = document.createElement('li');
+  const item = document.createElement('li');
   item.classList.add('item');
 
   // Left section of item
-  let left = document.createElement('div');
+  const left = document.createElement('div');
   left.classList.add('left');
 
-  let leftTop = document.createElement('div');
+  const leftTop = document.createElement('div');
   leftTop.classList.add('left-top');
 
-  let done = document.createElement('button');
+  const done = document.createElement('button');
   done.classList.add('done');
   done.addEventListener('click', completeItem);
+  const iconCheck = document.createElement('i');
+  iconCheck.classList.add('fas', 'fa-check');
+  done.appendChild(iconCheck);
 
-  let iconCheck = document.createElement('i');
-  iconCheck.classList.add('fas');
-  iconCheck.classList.add('fa-check');
-  var category = document.createElement('div');
+  const category = document.createElement('div');
   category.classList.add('category');
+  category.innerHTML = categoryIcon;
 
-  let iconCategory = document.createElement('i');
-  iconCategory.classList.add('fas');
-  iconCategory.classList.add('fa-home');
+  const iconCategory = document.createElement('i');
+  iconCategory.classList.add('fas', 'fa-home');
 
-  let task = document.createElement('div');
+  const task = document.createElement('div');
   task.classList.add('task');
 
-  let taskValue = document.createElement('p');
+  const taskValue = document.createElement('p');
   taskValue.classList.add('task-value');
   taskValue.innerHTML = text;
-  let desc = document.createElement('div');
+  const desc = document.createElement('div');
   desc.classList.add('description-text');
 
-  let descValue = document.createElement('p');
+  const descValue = document.createElement('p');
   descValue.classList.add('desc-value');
   descValue.innerHTML = description;
 
   // Right section of item
-  let right = document.createElement('div');
+  const right = document.createElement('div');
   right.classList.add('right');
 
-  let deleteBtn = document.createElement('button');
+  const deleteBtn = document.createElement('button');
   deleteBtn.classList.add('delete');
   deleteBtn.addEventListener('click', removeItem);
+  const deleteIcon = document.createElement('i');
+  deleteIcon.classList.add('fas', 'fa-times');
+  deleteBtn.appendChild(deleteIcon);
 
-  let deleteIcon = document.createElement('i');
-  deleteIcon.classList.add('fas');
-  deleteIcon.classList.add('fa-times');
-  let showBtn = document.createElement('button');
+  const showBtn = document.createElement('button');
   showBtn.classList.add('description');
   showBtn.addEventListener('click', showDescription);
-
-  let showIcon = document.createElement('i');
-  showIcon.classList.add('fas');
-  showIcon.classList.add('fa-ellipsis-h');
+  const showIcon = document.createElement('i');
+  showIcon.classList.add('fas', 'fa-ellipsis-h');
+  showBtn.appendChild(showIcon);
 
   item.appendChild(left);
   item.appendChild(right);
-
   left.appendChild(leftTop);
   left.appendChild(task);
   left.appendChild(desc);
-
   leftTop.appendChild(done);
   leftTop.appendChild(category);
-
-  done.appendChild(iconCheck);
-
-  category.innerHTML = categoryIcon;
-
   task.appendChild(taskValue);
-
   desc.appendChild(descValue);
-
   right.appendChild(deleteBtn);
   right.appendChild(showBtn);
-
-  deleteBtn.appendChild(deleteIcon);
-
-  showBtn.appendChild(showIcon);
 
   list.insertBefore(item, list.childNodes[0]);
 };
@@ -300,28 +287,32 @@ for (let i = 0; i < panelInputs.length; i++) {
 
 // === SWITCH ON/OFF DAY/NIGHT MODE ===
 const checkbox = document.querySelector('#switch-checkbox').addEventListener('change', switchColor);
-
 function switchColor() {
+  let setProp = (first, second) => {
+    document.documentElement.style.setProperty(first, second);
+  };
+
+  const varClrs = ['--first-color', '--second-color', '--bg-color', '--shadow-color', '--item-color'];
+  const darkClrs = ['#fff', '#000', '#111', '#111', 'rgb(24, 24, 24)'];
+  const lightClrs = ['#000', '#fff', 'rgb(250, 250, 250)', 'rgba(44, 62, 80, 0.10)', '#fff'];
+
   if (this.checked) {
-    document.documentElement.style.setProperty('--first-color', '#fff');
-    document.documentElement.style.setProperty('--second-color', '#000');
-    document.documentElement.style.setProperty('--bg-color', '#111');
-    document.documentElement.style.setProperty('--shadow-color', '#111');
-    document.documentElement.style.setProperty('--item-color', 'rgb(24, 24, 24)');
+    varClrs.forEach((clr, index) => {
+      setProp(clr, darkClrs[index]);
+    });
+
   } else {
-    document.documentElement.style.setProperty('--first-color', '#000');
-    document.documentElement.style.setProperty('--second-color', '#fff');
-    document.documentElement.style.setProperty('--bg-color', 'rgb(250, 250, 250)');
-    document.documentElement.style.setProperty('--shadow-color', 'rgba(44, 62, 80, 0.10)');
-    document.documentElement.style.setProperty('--item-color', '#fff');
+    varClrs.forEach((clr, index) => {
+      setProp(clr, lightClrs[index]);
+    });
   };
 };
 
 // === PRINT TIME AND DATE ===
 function printTime() {
-  let dayName = document.querySelector('.day');
-  let clock = document.querySelector('.clock');
-  let date = document.querySelector('.date');
+  const dayName = document.querySelector('.day');
+  const clock = document.querySelector('.clock');
+  const date = document.querySelector('.date');
   const now = new Date();
   let year = now.getFullYear();
   let hours = now.getHours();
